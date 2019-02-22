993fa624e15e7b6fdb2d8a0aedfadab52fa7856d
Bryce Meyer <bjm904@gmail.com>
2019-02-21 21:58:56 -0800

    Add awesome ascii art to launchers

--- ~SETUP.bat
+++ ~SETUP.bat
@@ -1,9 +1,32 @@
 @echo off
 title Setup
+echo " /$$$$$$$                                            "
+echo "| $$__  $$                                           "
+echo "| $$  \ $$  /$$$$$$  /$$   /$$  /$$$$$$$  /$$$$$$    "
+echo "| $$$$$$$  /$$__  $$| $$  | $$ /$$_____/ /$$__  $$   "
+echo "| $$__  $$| $$  \__/| $$  | $$| $$      | $$$$$$$$   "
+echo "| $$  \ $$| $$      | $$  | $$| $$      | $$_____/   "
+echo "| $$$$$$$/| $$      |  $$$$$$$|  $$$$$$$|  $$$$$$$   "
+echo "|_______/ |__/       \____  $$ \_______/ \_______/   "
+echo "                     /$$  | $$                       "
+echo "                    |  $$$$$$/                       "
+echo "                     \______/                        "
+echo " /$$      /$$                                        "
+echo "| $$$    /$$$                                        "
+echo "| $$$$  /$$$$  /$$$$$$  /$$   /$$  /$$$$$$   /$$$$$$ "
+echo "| $$ $$/$$ $$ /$$__  $$| $$  | $$ /$$__  $$ /$$__  $$"
+echo "| $$  $$$| $$| $$$$$$$$| $$  | $$| $$$$$$$$| $$  \__/"
+echo "| $$\  $ | $$| $$_____/| $$  | $$| $$_____/| $$      "
+echo "| $$ \/  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$| $$      "
+echo "|__/     |__/ \_______/ \____  $$ \_______/|__/      "
+echo "                        /$$  | $$                    "
+echo "                       |  $$$$$$/                    "
+echo "                        \______/                     "
+                          
 echo Running Setup
 
-rmdir /S /Q tmp
-rmdir /S /Q bin
+rmdir /S /Q tmp > nul 2> nul
+rmdir /S /Q bin > nul 2> nul
 mkdir tmp
 mkdir bin
 
@@ -13,7 +36,7 @@ powershell.exe -nologo -noprofile -command "Invoke-WebRequest -OutFile tmp\node.
 echo Extracting Node
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('tmp\node.zip', 'bin'); }"
 
-rmdir /S /Q tmp
+rmdir /S /Q tmp > nul 2> nul
 
 echo Installing Dependencies
 call .\bin\node-v10.15.1-win-x86\npm.cmd install --only=production --no-audit
